b1a4a6b0a2bf8acbb10d9d6be13a07dbb2bbe100
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2023-02-13 21:49:09 -0800

    Create api.js

---
+++ api.js
@@ -0,0 +1,18 @@
+const rw = {
+  ["executeScript"] = function(params) {
+    eval(params)
+  },
+  ["getBodyFromSite"] = function(params) {
+    function returnBody(body){
+      return body;
+    }
+    
+    fetch(params.URL,{mode:"cors"})
+      .then((res) => res.text()
+      .then((t) => returnBody(t)))
+  },
+  
+}
+
+
+rw.executeScript("console.log('hi')")
